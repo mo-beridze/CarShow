5d4478141c73b6937aac81df1a8d58f43b438ef6
mo-beridze <mokhamed04@gmail.com>
2023-06-20 00:17:18 +0300

    add CarDetails

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,17 +1,23 @@
 "use client";
 
+import { useState } from "react";
 import { CarProps } from "@/types";
 import Image from "next/image";
 import CustomButton from "./CustomButton";
 import { calculateCarRent } from "@/utils";
+import CarDetails from "./CarDetails";
 
 interface CarCardProps {
   car: CarProps;
 }
 
-function CarCard({ car }: CarCardProps) {
-  const { city_mpg, year, model, make, drive, transmission } = car;
+const CarCard = ({ car }: CarCardProps) => {
+  const { city_mpg, year, make, model, transmission, drive } = car;
+
+  const [isOpen, setIsOpen] = useState(false);
+
   const carRent = calculateCarRent(city_mpg, year);
+
   return (
     <div className="car-card group">
       <div className="car-card__content">
@@ -19,29 +25,61 @@ function CarCard({ car }: CarCardProps) {
           {make} {model}
         </h2>
       </div>
-      <p className="flex mt-6 text-[32px] font-extrabold">
-        <span className="self-start text-[14px] font-semibold">$</span>
+
+      <p className="flex mt-6 text-[32px] leading-[38px] font-extrabold">
+        <span className="self-start text-[14px] leading-[17px] font-semibold">$</span>
         {carRent}
-        <span className="self-end text-[14px] font-medium">/day</span>
+        <span className="self-end text-[14px] leading-[17px] font-medium">/day</span>
       </p>
-      <div className="w-full relative my-3 h-40 object-contain">
-        <Image src="/hero.png" alt="Car model" fill priority className="object-contain" />
+
+      <div className="relative w-full h-40 my-3 object-contain">
+        <Image
+          // src={generateCarImageUrl(car)}
+          src="/hero.png"
+          alt="car model"
+          fill
+          priority
+          className="object-contain"
+        />
       </div>
+
       <div className="relative flex w-full mt-2">
-        <div className="flex group-hover:invisible text-gray w-full justify-between">
+        <div className="flex group-hover:invisible w-full justify-between text-grey">
           <div className="flex flex-col justify-center items-center gap-2">
             <Image
               src="/steering-wheel.svg"
-              alt="steering wheel"
               width={20}
               height={20}
+              alt="steering wheel"
             />
-            <p className="text-[14px]">{transmission === "a" ? "Automatic" : "Manual"}</p>
+            <p className="text-[14px] leading-[17px]">
+              {transmission === "a" ? "Automatic" : "Manual"}
+            </p>
+          </div>
+          <div className="car-card__icon">
+            <Image src="/tire.svg" width={20} height={20} alt="seat" />
+            <p className="car-card__icon-text">{drive.toUpperCase()}</p>
+          </div>
+          <div className="car-card__icon">
+            <Image src="/gas.svg" width={20} height={20} alt="seat" />
+            <p className="car-card__icon-text">{city_mpg} MPG</p>
           </div>
         </div>
+
+        <div className="car-card__btn-container">
+          <CustomButton
+            title="View More"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(true)}
+          />
+        </div>
       </div>
+
+      <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car} />
     </div>
   );
-}
+};
 
 export default CarCard;
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -8,6 +8,8 @@ function CustomButton({
   containerStyles,
   handleClick,
   btnType,
+  textStyles,
+  rightIcon,
 }: CustomButtonProps) {
   return (
     <button
@@ -16,7 +18,12 @@ function CustomButton({
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      {title}
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image src={rightIcon} alt="arrow_left" fill className="object-contain" />
+        </div>
+      )}
     </button>
   );
 }
